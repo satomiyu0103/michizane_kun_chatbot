508b8ee3ac2f935d005c46d3693ff58084d0aaa5
yuro0103 <211184636+satomiyu0103@users.noreply.github.com>
2025-07-18 11:21:34 +0900

    https化に対応

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from flask import Flask
 # Flaskアプリのインスタンスを作成
 app = Flask(__name__)
 
+
 # '/' という一番シンプルなURLにアクセスがあった時の処理
-@app.route('/')
+@app.route("/")
 def hello():
     # 「Hello, Michizane-kun!」という文字を返す
-    return 'Hello, Michizane-kun!'
+    return "Hello, Michizane-kun!"
+
 
 # このファイルが直接実行された場合にのみ、Webサーバーを起動する
-if __name__ == '__main__':
+if __name__ == "__main__":
     # デバッグモードを有効にしてサーバーを起動
     # ※デバッグモード：コードを変更すると自動で再起動してくれたり、エラー表示が親切になったりする開発用の便利モード
-    app.run(debug=True)
+    app.run(debug=True, ssl_context=("cert.pem", "key.pem"))
