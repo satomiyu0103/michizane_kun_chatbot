18aede84025d61f992d8b9fcc594e1a4b1d68c77
yuro0103 <211184636+satomiyu0103@users.noreply.github.com>
2025-08-27 14:38:37 +0900

    mini_runnerの更新

---
+++ main_py/mini_runner.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from modules.test_gemini_handler import ask_gemini
+
+
+def run(user_text, rules_summary):
+    GENAI_API_KEY, MODEL_NAME = get_keys()
+    global _model
+    if _model is None:
+        _model = _get_model(GENAI_API_KEY, MODEL_NAME)
+    ask_gemini(_model, user_text, rules_summary)
+
+
+user_text = """
+福岡の有名グルメと言えば？
+    """
+rules_summary = ""
+
+if __name__ == "__main__":
+    run(user_text, rules_summary)
