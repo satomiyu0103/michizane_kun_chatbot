0dde644547b83dcae40b9db75d1e5da04477c0dd
yuro0103 <211184636+satomiyu0103@users.noreply.github.com>
2025-08-29 13:54:50 +0900

    spreadsheet_handler草案を作成

---
+++ modules/spreadsheet_handler.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+def csv_url(sheet_name):
+    load_dotenv()
+    SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+    GID = {
+        "faq": 1932958690,
+        "spots": 0,
+        "tags": 1863403900,
+        "categories": 88251615,
+    }
+    return f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?format=csv&gid={GID[sheet_name]}"
+
+
+def load_spresheet():
+    url_faq = csv_url("faq")
+    url_spots = csv_url("spots")
+    url_tags = csv_url("tags")
+    url_categories = csv_url("categories")
+    df_faq = pd.read_csv(url_faq)
+    df_spots = pd.read_csv(url_spots)
+    df_tags = pd.read_csv(url_tags)
+    df_categories = pd.read_csv(url_categories)
+    return df_faq, df_spots, df_tags, df_categories
+
+
+def get_rule_text():
+    df_faq, df_spots, df_tags, df_categories = load_spresheet()
+    text_faq = df_faq.to_string(index=False)
+    text_spots = df_spots.to_string(index=False)
+    text_tags = df_tags.to_string(index=False)
+    text_categories = df_categories.to_string(index=False)
+    return text_faq, text_spots, text_tags, text_categories
